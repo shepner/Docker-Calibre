5a1504432b076fc78c46e70b17272c4fa1b119bf
Stephen Hepner <5170785+shepner@users.noreply.github.com>
2019-02-01 21:53:50 -0600

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -7,4 +7,5 @@ x11vnc -display :1 -nopw -listen localhost -xkb -ncache 10 -ncache_cr -forever &
 
 #cd /root/noVNC && ln -s vnc_auto.html index.html && ./utils/launch.sh --vnc localhost:5900
 
+ln -s /root/noVNC/vnc_lite.html /root/noVNC/index.html
 /root/noVNC/utils/launch.sh --vnc localhost:5900
