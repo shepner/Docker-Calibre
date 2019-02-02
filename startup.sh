b1d13faaea9b34a660fd10ccd8cfe061cf3dd2b3
Stephen Hepner <5170785+shepner@users.noreply.github.com>
2019-02-01 22:43:43 -0600

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -4,12 +4,15 @@ Xvfb :1 -screen 0 1600x900x16 &
 sleep 5
 x11vnc -display :1 -nopw -listen localhost -xkb -ncache 10 -ncache_cr -forever &
 
-openbox-session &  #window manager
+
+#install the latest version of calibre (which updates frequently)
+wget -nv -O- https://download.calibre-ebook.com/linux-installer.sh | sudo sh /dev/stdin
+
+#launch apps
+openbox-session &  # window manager
 #firefox &
+/opt/calibre/calibre &
 
-wget -nv -O- https://raw.githubusercontent.com/kovidgoyal/calibre/master/setup/linux-installer.py | sudo python -c "import sys; main=lambda:sys.stderr.write('Download failed\n'); exec(sys.stdin.read()); main()"
-
-#screen -d -m /opt/calibre/calibre
 
 ln -s /root/noVNC/vnc_lite.html /root/noVNC/index.html
 /root/noVNC/utils/launch.sh --vnc localhost:5900
