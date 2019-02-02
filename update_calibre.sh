d99917c34a5efa5dfbee61564b62d15c058c6258
Stephen Hepner <5170785+shepner@users.noreply.github.com>
2019-02-01 23:47:57 -0600

    Delete update_calibre.sh

--- update_calibre.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-#update calibre
-wget -nv -O- https://raw.githubusercontent.com/kovidgoyal/calibre/master/setup/linux-installer.py | sudo python -c "import sys; main=lambda:sys.stderr.write('Download failed\n'); exec(sys.stdin.read()); main()"
-
