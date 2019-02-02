0b01f69107edd1b1a2acf6971b8ad25f7fef3891
Stephen Hepner <5170785+shepner@users.noreply.github.com>
2019-02-01 22:28:58 -0600

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -2,10 +2,12 @@
 export DISPLAY=:1
 Xvfb :1 -screen 0 1600x900x16 &
 sleep 5
-#openbox-session &
 x11vnc -display :1 -nopw -listen localhost -xkb -ncache 10 -ncache_cr -forever &
 
-firefox &
+openbox-session &  #window manager
+#firefox &
+/root/update_calibre.sh
+#screen -d -m /opt/calibre/calibre
 
 ln -s /root/noVNC/vnc_lite.html /root/noVNC/index.html
 /root/noVNC/utils/launch.sh --vnc localhost:5900
