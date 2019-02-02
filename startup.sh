8edd5eec845b1b9a9816a2314e2c6f5845f27e9f
Stephen Hepner <5170785+shepner@users.noreply.github.com>
2019-02-01 22:11:19 -0600

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -2,7 +2,7 @@
 export DISPLAY=:1
 Xvfb :1 -screen 0 1600x900x16 &
 sleep 5
-openbox-session &
+#openbox-session &
 x11vnc -display :1 -nopw -listen localhost -xkb -ncache 10 -ncache_cr -forever &
 
 firefox &
