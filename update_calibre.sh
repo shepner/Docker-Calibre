5750a6f1a2613c836323fb5262b52ded2c1487a6
Stephen Hepner <5170785+shepner@users.noreply.github.com>
2019-02-01 20:21:07 -0600

    Create update_calibre.sh

---
+++ update_calibre.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+#update calibre
+wget -nv -O- https://raw.githubusercontent.com/kovidgoyal/calibre/master/setup/linux-installer.py | sudo python -c "import sys; main=lambda:sys.stderr.write('Download failed\n'); exec(sys.stdin.read()); main()"
+
