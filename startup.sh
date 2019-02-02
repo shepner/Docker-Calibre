7c2f7fcb15fb5c8b725cc26cc7b30277fc5d90fe
Stephen Hepner <5170785+shepner@users.noreply.github.com>
2019-02-02 09:45:12 -0600

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-export DISPLAY=:1
+#export DISPLAY=:1
 Xvfb :1 -screen 0 1600x900x16 &
 sleep 5
 x11vnc -display :1 -nopw -listen localhost -xkb -ncache 10 -ncache_cr -forever &
-
 
 #install the latest version of calibre (which updates frequently)
 wget -nv -O- https://download.calibre-ebook.com/linux-installer.sh | sudo sh /dev/stdin
@@ -13,6 +12,4 @@ openbox-session &  # window manager
 #firefox &
 /opt/calibre/calibre &
 
-
-ln -s /root/noVNC/vnc_lite.html /root/noVNC/index.html
 /root/noVNC/utils/launch.sh --vnc localhost:5900
