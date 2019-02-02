9681280033706c7527293e49733c76b80eece7cd
Stephen Hepner <5170785+shepner@users.noreply.github.com>
2019-02-01 22:32:12 -0600

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -6,7 +6,9 @@ x11vnc -display :1 -nopw -listen localhost -xkb -ncache 10 -ncache_cr -forever &
 
 openbox-session &  #window manager
 #firefox &
-/root/update_calibre.sh
+
+wget -nv -O- https://raw.githubusercontent.com/kovidgoyal/calibre/master/setup/linux-installer.py | sudo python -c "import sys; main=lambda:sys.stderr.write('Download failed\n'); exec(sys.stdin.read()); main()"
+
 #screen -d -m /opt/calibre/calibre
 
 ln -s /root/noVNC/vnc_lite.html /root/noVNC/index.html
