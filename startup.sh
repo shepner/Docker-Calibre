d3e87646a9b2fa2d96dfd6186e0cccb92fceaa41
Stephen Hepner <5170785+shepner@users.noreply.github.com>
2019-02-02 19:12:50 -0600

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #export DISPLAY=:1
-Xvfb :1 -screen 0 1600x900x16 &
+Xvfb :1 -screen 0 1280x740x16 &
 sleep 5
 x11vnc -display :1 -nopw -listen localhost -xkb -ncache 10 -ncache_cr -forever &
 
