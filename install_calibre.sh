bbeb5638872b96780d67e1b103d59fb496026ef1
Stephen Hepner <5170785+shepner@users.noreply.github.com>
2019-02-02 18:36:27 -0600

    Update install_calibre.sh

--- install_calibre.sh
+++ install_calibre.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 #install the latest version of calibre (which updates frequently)
-wget -nv -O- https://download.calibre-ebook.com/linux-installer.sh | sudo sh /dev/stdin
+#wget -nv -O- https://download.calibre-ebook.com/linux-installer.sh | sudo sh /dev/stdin
+wget -nv -O- https://download.calibre-ebook.com/linux-installer.sh | sh /dev/stdin install_dir=~/calibre isolated=y
 
