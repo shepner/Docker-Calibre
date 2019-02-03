67a2959a30f8a00cffad4b48e68b36c2fee5e1da
Stephen Hepner <5170785+shepner@users.noreply.github.com>
2019-02-02 18:49:29 -0600

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -4,12 +4,8 @@ Xvfb :1 -screen 0 1600x900x16 &
 sleep 5
 x11vnc -display :1 -nopw -listen localhost -xkb -ncache 10 -ncache_cr -forever &
 
-#install the latest version of calibre (which updates frequently)
-wget -nv -O- https://download.calibre-ebook.com/linux-installer.sh | sudo sh /dev/stdin
-
 #launch apps
 openbox-session &  # window manager
-#firefox &
 
 $HOME/install_calibre.sh
 $HOME/calibre/calibre &
