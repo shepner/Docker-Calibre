b48058e44d8d84102d1c7bb0e3ebf0bb8bb86a63
Stephen Hepner <5170785+shepner@users.noreply.github.com>
2019-12-27 08:40:55 -0600

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 #export DISPLAY=:1
 Xvfb :1 -screen 0 1280x740x16 &
-sleep 15
+
+$HOME/install_calibre.sh
+
+#Xvfb takes a while to start and needs to complete before x11vnc can be launched
+#sleep 15
 x11vnc -display :1 -nopw -listen localhost -xkb -ncache 10 -ncache_cr -forever &
 
 #launch apps
 openbox-session &  # window manager
 
-$HOME/install_calibre.sh
 $HOME/calibre/calibre &
 
 $HOME/noVNC/utils/launch.sh --vnc localhost:5900
