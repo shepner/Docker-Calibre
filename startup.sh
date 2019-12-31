1136481686300efe87bc01f3f8467ed9044a2170
Stephen Hepner <5170785+shepner@users.noreply.github.com>
2019-12-30 21:15:15 -0600

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -2,15 +2,17 @@
 #export DISPLAY=:1
 Xvfb :1 -screen 0 1280x740x16 &
 
-#$HOME/install_calibre.sh
+$HOME/install_calibre.sh
 
 #Xvfb takes a while to start and needs to complete before x11vnc can be launched
 #sleep 15
-#x11vnc -display :1 -nopw -listen localhost -xkb -ncache 10 -ncache_cr -forever &
+x11vnc -display :1 -nopw -listen localhost -xkb -ncache 10 -ncache_cr -forever &
 
 #launch apps
-#openbox-session &  # window manager
+openbox-session &  # window manager
 
-#$HOME/calibre/calibre &
+#https://github.com/therecipe/qt/issues/775
+export QT_QPA_PLATFORM=offscreen
+$HOME/calibre/calibre &
 
-#$HOME/noVNC/utils/launch.sh --vnc localhost:5900
+$HOME/noVNC/utils/launch.sh --vnc localhost:5900
